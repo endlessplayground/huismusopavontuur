97e7e53cb7dede9a03e2e9f5b595554f59d06dd2
Lady 1965 <unfinishedthoughts1965@gmail.com>
2026-09-07 20:13:54 +0200

    Refactor dice roll animation and image paths
    Updated dice rolling logic to improve animation timing and fixed image paths.

--- js/random.js
+++ js/random.js
@@ -3,31 +3,33 @@ const diceImage = dice.querySelector('img');
 const output = document.getElementById('output');
 
 const diceImages = [
-  '../images/dice1.svg',
-  '../images/dice2.svg',
-  '../images/dice3.svg',
-  '../images/dice4.svg',
-  '../images/dice5.svg',
-  '../images/dice6.svg'
+  '/images/dice1.svg',
+  '/images/dice2.svg',
+  '/images/dice3.svg',
+  '/images/dice4.svg',
+  '/images/dice5.svg',
+  '/images/dice6.svg'
 ];
 
 dice.addEventListener('click', () => {
   output.innerHTML = "";
 
-  // Choose the next dice face BEFORE the roll
-  const randomDice =
-    diceImages[Math.floor(Math.random() * diceImages.length)];
-
-  // Put the new face in place while the dice is still rolling
-  diceImage.src = randomDice;
-
+  // Restart the animation
   dice.classList.remove('roll-animation');
   void dice.offsetWidth;
   dice.classList.add('roll-animation');
 
+  // Choose the final dice face
+  const randomDice =
+    diceImages[Math.floor(Math.random() * diceImages.length)];
+
+  // Change it shortly after the roll has started
+  setTimeout(() => {
+    diceImage.src = randomDice;
+  }, 100);
+
   setTimeout(() => {
 
-    // Choose a random text
     const randomFile =
       randomContent[Math.floor(Math.random() * randomContent.length)];
 
@@ -35,6 +37,14 @@ dice.addEventListener('click', () => {
       '<em>Nog</em> een keer<em>?</em>';
 
     fetch(randomFile)
+      .then(response => response.text())
+      .then(html => {
+        output.innerHTML = html;
+        dice.classList.remove('roll-animation');
+      });
+
+  }, 700);
+});    fetch(randomFile)
       .then(response => response.text())
       .then(html => {
         output.innerHTML = html;
