572ab66c073e4791c95a4b9ab1d6c001b3bb901d
Lady 1965 <unfinishedthoughts1965@gmail.com>
2026-09-08 05:17:37 +0200

    Change dice image paths to relative URLs
    Updated image paths for dice images to relative paths.

--- js/random.js
+++ js/random.js
@@ -3,12 +3,12 @@ const diceImage = document.querySelector('#roll img');
 const output = document.getElementById('output');
 
 const diceImages = [
-  '/images/dice1.svg',
-  '/images/dice2.svg',
-  '/images/dice3.svg',
-  '/images/dice4.svg',
-  '/images/dice5.svg',
-  '/images/dice6.svg'
+  '../images/dice1.svg',
+  '../images/dice2.svg',
+  '../images/dice3.svg',
+  '../images/dice4.svg',
+  '../images/dice5.svg',
+  '../images/dice6.svg'
 ];
 
 // Preload all dice images
